f7a87dafd46a38ccf5f7ce2d42dec404534707f4
0xInnominatus <259166792+0xInnominatus@users.noreply.github.com>
2026-02-06 16:11:13 +0300

    Fix fmt

--- contracts/AssetVault.sol
+++ contracts/AssetVault.sol
@@ -4,7 +4,9 @@ import {ERC4626} from "@openzeppelin-contracts-5.4.0/token/ERC20/extensions/ERC4
 import {IERC20Metadata} from "@openzeppelin-contracts-5.4.0/token/ERC20/extensions/IERC20Metadata.sol";
 import {ERC20} from "@openzeppelin-contracts-5.4.0/token/ERC20/ERC20.sol";
 
-contract AssetVault is ERC4626
-{
-	constructor(IERC20Metadata asset) ERC4626(asset) ERC20(string.concat("bno", asset.name()), string.concat("bno", asset.symbol())) {}
+contract AssetVault is ERC4626 {
+	constructor(IERC20Metadata asset)
+		ERC4626(asset)
+		ERC20(string.concat("bno", asset.name()), string.concat("bno", asset.symbol()))
+	{}
 }
