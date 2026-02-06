5a03944dc0bf4e3a0f0d10b1bb2659dbafef8cd3
0xInnominatus <259166792+0xInnominatus@users.noreply.github.com>
2026-02-06 16:08:17 +0300

    Downgrade version of OZ

--- contracts/AssetVault.sol
+++ contracts/AssetVault.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.8.23;
 
-import {ERC4626} from "@openzeppelin-contracts/token/ERC20/extensions/ERC4626.sol";
+import {ERC4626} from "@openzeppelin-contracts-5.4.0/token/ERC20/extensions/ERC4626.sol";
+import {IERC20Metadata} from "@openzeppelin-contracts-5.4.0/token/ERC20/extensions/IERC20Metadata.sol";
+import {ERC20} from "@openzeppelin-contracts-5.4.0/token/ERC20/ERC20.sol";
 
 contract AssetVault is ERC4626
 {
-	constructor(address asset, string memory name, string memory symbol) ERC4626(asset, name, symbol) {}
+	constructor(IERC20Metadata asset) ERC4626(asset) ERC20(string.concat("bno", asset.name()), string.concat("bno", asset.symbol())) {}
 }
