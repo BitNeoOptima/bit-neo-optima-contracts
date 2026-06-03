1c277936a002dc7acf7b9fb9aa437b58b1696682
0xInnominatus <259166792+0xInnominatus@users.noreply.github.com>
2026-06-03 18:44:21 -0300

    Add support of tests at mainnet fork

---
+++ tests/mainnet-fork-tests/test_distribution.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+from wake.testing import *
+from pytypes.contracts.MatrixVault import MatrixVault
+from pytypes.contracts.AssetVault import AssetVault
+from pytypes.wake.interfaces.IERC20 import IERC20
+
+load_dotenv()
+
+WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+DEPOSIT_AMOUNT = 50 * 10**18
+
+
+@chain.connect(
+	accounts=2,
+	chain_id=1,
+	fork=os.getenv("ETHEREUM_RPC_URL"),
+)
+def test_distribution():
+	user = chain.accounts[1]
+	matrix_vault = MatrixVault.deploy()
+	asset_vault = AssetVault.deploy(WETH)
+	assert asset_vault.asset() == WETH
+	# WETH9: plain ETH transfer to the contract mints WETH
+	Account(WETH).transact(value=DEPOSIT_AMOUNT, from_=user)
+	weth = IERC20(WETH)
+	weth.approve(asset_vault.address, DEPOSIT_AMOUNT, from_=user)
+	asset_vault.deposit(DEPOSIT_AMOUNT, user.address, from_=user)
+	assert asset_vault.balanceOf(user.address) == DEPOSIT_AMOUNT
+	assert asset_vault.totalAssets() == DEPOSIT_AMOUNT
